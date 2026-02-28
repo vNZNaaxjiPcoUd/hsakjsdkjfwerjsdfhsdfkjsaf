f07bb8d79d8e77c3d2da5b1f7dc1fd0146d820f1
Very Good Sky <92081049+vNZNaaxjiPcoUd@users.noreply.github.com>
2026-02-28 15:19:16 -0500

    dd

---
+++ docs/ttt/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'morris-game-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 安裝時快取資源
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// 攔截請求並提供快取內容
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// 清理舊快取
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
